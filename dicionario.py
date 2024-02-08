1d074488ba6cb766708c70c80ab9911ed1fbdfc6
Wesley Freire <wesleyfreire1707@gmail.com>
2024-02-07 22:30:16 -0300

    new beginning

--- dicionario.py
+++ dicionario.py
@@ -10,4 +10,7 @@ player = {
 #lista de inimigos
 npcs = [
     {"nome": "monstrinho", "dano": 2, "hp": 50, "exp": 5},
+    {"nome": "monstro", "dano": 5, "hp": 100, "exp": 10},
+    {"nome": "monstrão", "dano": 10, "hp": 150, "exp": 15},
+    {"nome": "chefão", "dano": 25, "hp": 200, "exp": 20},
 ]
