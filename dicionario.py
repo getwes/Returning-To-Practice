2b0ce71ba1e78893375441523f1b7a6c591e728c
Wesley Freire <wesleyfreire1707@gmail.com>
2024-02-07 22:24:49 -0300

    new beginning

---
+++ dicionario.py
@@ -0,0 +1,13 @@
+# informção do jogador
+player = {
+    "nome": "wesley",
+    "level": 1,
+    "hp": 100,
+    "exp": 0,
+    "dano": 5,
+}
+
+#lista de inimigos
+npcs = [
+    {"nome": "monstrinho", "dano": 2, "hp": 50, "exp": 5},
+]
