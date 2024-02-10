c0450332050aa75914f3869c106fc6bb50b6e6b4
Wesley Freire <wesleyfreire1707@gmail.com>
2024-02-10 18:11:00 -0300

    new beginning

--- chat.py
+++ chat.py
@@ -1,1 +1,16 @@
 import os 
+
+mensagens = []
+
+nome = input("nome:")
+
+while True:
+
+  #limpando terminal
+    os.system('cls')
+
+    if len(mensagens) > 0:
+        for m in mensagens:
+            print(m['nome'], "-", m['texto'])
+            
+    print("-------------")
